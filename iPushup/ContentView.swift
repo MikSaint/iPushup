5d8ba60fbc3cae9fc3aea2187e31e617d4dc3c4c
Mikhail St-Denis <oneSaint@gmail.com>
2023-09-30 12:14:25 +0100

    Initial Commit

---
+++ iPushup/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iPushup
+//
+//  Created by Mikhail St-Denis on 9/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iPushup/iPushupApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPushupApp.swift
+//  iPushup
+//
+//  Created by Mikhail St-Denis on 9/30/23.
+//
+
+import SwiftUI
+
+@main
+struct iPushupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
